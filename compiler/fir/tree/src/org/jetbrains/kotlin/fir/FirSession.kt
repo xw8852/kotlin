2cf6b39f1e0a19a6fdc9d86bf58bdac0808cfc2a
Denis Zharkov <denis.zharkov@jetbrains.com>
2019-08-14 12:54:55 +0300

    Rename Jsr305State -> JavaTypeEnhancementState
    Also, rename some of the properties
    It's needed to store status for codeanalysis annotation in that class

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/FirSession.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/FirSession.kt
@@ -12,6 +12,8 @@ import org.jetbrains.kotlin.fir.utils.ArrayMapAccessor
 import org.jetbrains.kotlin.fir.utils.ComponentArrayOwner
 import org.jetbrains.kotlin.fir.utils.TypeRegistry
 import org.jetbrains.kotlin.utils.Jsr305State
+import org.jetbrains.kotlin.utils.JavaTypeEnhancementState
+import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KClass
 
 interface FirSessionComponent
@@ -25,7 +27,7 @@ abstract class FirSession(val sessionProvider: FirSessionProvider?) : ComponentA
 
     open val moduleInfo: ModuleInfo? get() = null
 
-    val jsr305State: Jsr305State? get() = null
+    val javaTypeEnhancementState: JavaTypeEnhancementState? get() = null
 
     val builtinTypes: BuiltinTypes = BuiltinTypes()
 
