c8039bf8170fb5336cf355b2ed9b58062275b410
Victor Petukhov <victor.petukhov@jetbrains.com>
2020-07-28 14:58:59 +0300

    1

--- third-party/jdk8-annotations/jspecify/annotations/NullnessUnknown.java
+++ third-party/jdk8-annotations/jspecify/annotations/NullnessUnknown.java
@@ -1,9 +1,9 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package codeanalysis.experimental.annotations;
+package jspecify.annotations;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
