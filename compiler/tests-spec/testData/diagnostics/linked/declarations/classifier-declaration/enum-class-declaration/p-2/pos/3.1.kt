d8bc2f9f7cc855b37d667a3374a0cbd774dc48f0
anastasiia.spaseeva <anastasiia.spaseeva@jetbrains.com>
2020-08-18 13:54:49 +0300

    [Spec tests] Enum: add txt file to check implicitly overridden members

--- compiler/tests-spec/testData/diagnostics/linked/declarations/classifier-declaration/enum-class-declaration/p-2/pos/3.1.kt
+++ compiler/tests-spec/testData/diagnostics/linked/declarations/classifier-declaration/enum-class-declaration/p-2/pos/3.1.kt
@@ -1,6 +1,5 @@
 // !LANGUAGE: +NewInference
 // !DIAGNOSTICS: -UNUSED_VARIABLE -ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE -UNUSED_VALUE -UNUSED_PARAMETER -UNUSED_EXPRESSION
-// SKIP_TXT
 
 /*
  * KOTLIN DIAGNOSTICS SPEC TEST (POSITIVE)
@@ -8,6 +7,8 @@
  * SPEC VERSION: 1.4-rfc+0.3-603
  * MAIN LINK: declarations, classifier-declaration, enum-class-declaration -> paragraph 2 -> sentence 3
  * PRIMARY LINKS: declarations, classifier-declaration, enum-class-declaration -> paragraph 4 -> sentence 1
+ * declarations, classifier-declaration, enum-class-declaration -> paragraph 5 -> sentence 1
+ * declarations, classifier-declaration, enum-class-declaration -> paragraph 7 -> sentence 1
  * NUMBER: 1
  * DESCRIPTION: enum class implicitly inherits the built-in class kotlin.Enum<E>
  * HELPERS: checkType
