4eeff835e68764f79d32cb33242f845e6564954a
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-08-21 19:41:17 +0300

    ~~~ Extend FIR resolve test with delegated super type ~~~

--- compiler/fir/analysis-tests/testData/resolve/delegatedSuperType.kt
+++ compiler/fir/analysis-tests/testData/resolve/delegatedSuperType.kt
@@ -7,3 +7,25 @@ class B : A {
 }
 
 class C(val b: B) : A by b
+
+fun bar() {
+    val b = B()
+    val c = C(b)
+    c.foo()
+}
+
+interface AA<T> {
+    fun foo(x: T)
+}
+
+class BB<T>(val y: T) : AA<T> {
+    override fun foo(x: T) {}
+}
+
+class CC(val b: BB<String>) : AA<String> by b
+
+fun baz() {
+    val b = BB("")
+    val c = CC(b)
+    c.foo("")
+}
